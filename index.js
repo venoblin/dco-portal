d2224fa858d6ba92a4ed9416f8a42e44d38e72d8
Jonathan <villagomezhjonathan@gmail.com>
2025-09-20 15:07:14 -0400

    first commit of files

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.listen(PORT, () => {
+  console.log('Started on port: ' + PORT)
+})
