9726100840b06bff0577ee5c997558415c2545dd
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2025-09-21 00:04:25 -0400

    created profile

---
+++ frontend/src/routes/profile/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Panel from '$lib/components/ui/Panel.svelte'
+
+</script>
+
+<header>
+  <h1>Profile</h1>
+</header>
+
+<Panel>
+  <h2>User Guides</h2>
+  <p>User guides go here</p>
+</Panel>
