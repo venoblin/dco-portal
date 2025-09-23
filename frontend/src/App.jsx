fd144cce2630debd4c937c3eaf640a2309f52f29
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2025-09-23 17:11:34 -0400

    created routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,16 @@
+import { Route, Routes } from 'react-router-dom'
+import Dashboard from './routes/dashboard'
+import Guides from './routes/Guides'
+
 const App = () => {
-  return <div className="App">APP</div>
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/guides" element={<Guides />} />
+      </Routes>
+    </div>
+  )
 }
 
 export default App
