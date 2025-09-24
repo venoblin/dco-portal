92037883dde808c8b2e0e3e5b295c10b262e1ff4
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2025-09-24 19:46:39 -0400

    added more fonts

--- frontend/src/components/ContentEditor.jsx
+++ frontend/src/components/ContentEditor.jsx
@@ -1,14 +1,44 @@
 import 'draft-js/dist/Draft.css'
 import './ContentEditor.css'
 import { useState } from 'react'
-import { Editor, EditorState } from 'draft-js'
+import { Editor, EditorState, RichUtils } from 'draft-js'
 
-const ContentEditor = (props) => {
+const ContentEditor = () => {
   const [editorState, setEditorState] = useState(() =>
     EditorState.createEmpty()
   )
 
-  return <Editor editorState={editorState} onChange={setEditorState} />
+  const onToggleInlineStyle = (inlineStyle) => {
+    setEditorState(RichUtils.toggleInlineStyle(editorState, inlineStyle))
+  }
+
+  const handleKeyCommand = (command) => {
+    const newState = RichUtils.handleKeyCommand(editorState, command)
+    if (newState) {
+      setEditorState(newState)
+      return 'handled'
+    }
+    return 'not-handled'
+  }
+
+  return (
+    <div className="ContentEditor">
+      <div className="toolbar">
+        <button type="button" onClick={() => onToggleInlineStyle('BOLD')}>
+          Bold
+        </button>
+        <button type="button" onClick={() => onToggleInlineStyle('ITALIC')}>
+          Italic
+        </button>
+      </div>
+
+      <Editor
+        editorState={editorState}
+        onChange={setEditorState}
+        handleKeyCommand={handleKeyCommand}
+      />
+    </div>
+  )
 }
 
 export default ContentEditor
