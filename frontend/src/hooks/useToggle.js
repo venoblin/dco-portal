3719a098a6fc6b9d86b887341353bf4394516dc4
Jonathan <villagomezhjonathan@gmail.com>
2025-09-25 23:16:20 -0400

    created toggle state

---
+++ frontend/src/hooks/useToggle.js
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+const useToggle = (initial = false) => {
+  const [state, setState] = useState(initial)
+
+  const toggle = () => {
+    setState((currentState) => !currentState)
+  }
+
+  return [state, toggle]
+}
+
+export default useToggle
