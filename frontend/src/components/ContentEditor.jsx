378ce4c96fa4dc7b5408eee045e693c972dcedb1
Jonathan <villagomezhjonathan@gmail.com>
2025-09-25 23:13:51 -0400

    worked on color picker

--- frontend/src/components/ContentEditor.jsx
+++ frontend/src/components/ContentEditor.jsx
@@ -9,6 +9,57 @@ const ContentEditor = () => {
     EditorState.createEmpty()
   )
 
+  const styleMap = {
+    TEXT_BLACK: {
+      color: '#000000'
+    },
+    TEXT_RED: {
+      color: '#D93025'
+    },
+    TEXT_BLUE: {
+      color: '#1A73E8'
+    },
+    TEXT_GREEN: {
+      color: '#188038'
+    },
+    TEXT_PURPLE: {
+      color: '#673AB7'
+    },
+    TEXT_ORANGE: {
+      color: '#F9AB00'
+    },
+    TEXT_AQUA: {
+      color: '#00FFFF'
+    },
+    TEXT_FUCHSIA: {
+      color: '#FF00FF'
+    },
+    TEXT_LIME: {
+      color: '#00FF00'
+    },
+    TEXT_YELLOW: {
+      color: '#FFD700'
+    },
+    TEXT_CRIMSON: {
+      color: '#DC143C'
+    },
+    TEXT_GRAY_LIGHT: {
+      color: '#9AA0A6'
+    },
+    TEXT_GRAY_DARK: {
+      color: '#5F6368'
+    },
+    TEXT_BROWN: {
+      color: '#795548'
+    },
+    TEXT_INDIGO: {
+      color: '#3F51B5'
+    },
+    TEXT_WHITE: {
+      color: '#FFFFFF'
+    }
+  }
+
   const onToggleInlineStyle = (inlineStyle) => {
     setEditorState(RichUtils.toggleInlineStyle(editorState, inlineStyle))
   }
@@ -30,36 +81,38 @@ const ContentEditor = () => {
     <div className="ContentEditor">
       <div className="toolbar">
         <div className="btns-wrap">
-          <EditorBtn btnFor="bold" onClick={() => onToggleInlineStyle('BOLD')}>
-            Bold
-          </EditorBtn>
+          <EditorBtn
+            btnFor="bold"
+            onClick={() => onToggleInlineStyle('BOLD')}
+          />
+
           <EditorBtn
             btnFor="italic"
             onClick={() => onToggleInlineStyle('ITALIC')}
-          >
-            Italic
-          </EditorBtn>
+          />
+
           <EditorBtn
             btnFor="underline"
             onClick={() => onToggleInlineStyle('UNDERLINE')}
-          >
-            Underline
-          </EditorBtn>
+          />
+
           <EditorBtn
             btnFor="strikethrough"
             onClick={() => onToggleInlineStyle('STRIKETHROUGH')}
-          >
-            Strikethrough
-          </EditorBtn>
-        </div>
+          />
 
-        <div className="btns-wrap"></div>
+          <EditorBtn
+            btnFor="textColor"
+            onClick={() => onToggleInlineStyle('TEXT_RED')}
+          />
+        </div>
       </div>
 
       <Editor
         editorState={editorState}
         onChange={setEditorState}
         handleKeyCommand={handleKeyCommand}
+        customStyleMap={styleMap}
       />
     </div>
   )
