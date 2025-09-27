d41f3ce49b85c963353d34af5fd44f032546d0ec
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2025-09-27 14:20:45 -0400

    disabled author input

--- frontend/src/components/ContentEditor.jsx
+++ frontend/src/components/ContentEditor.jsx
@@ -1,192 +1,55 @@
-import 'draft-js/dist/Draft.css'
+import 'react-quill/dist/quill.snow.css'
 import './ContentEditor.css'
-import {
-  useState,
-  useEffect,
-  useRef,
-  forwardRef,
-  useImperativeHandle
-} from 'react'
-import { convertToRaw, Editor, EditorState, RichUtils } from 'draft-js'
-import editorStyleMap from '../utils/editorStyleMap'
-import useToggle from '../hooks/useToggle'
-import EditorBtn from './EditorBtn'
-import EditorColorPicker from './EditorColorPicker'
+import { useState, forwardRef, useImperativeHandle } from 'react'
+import ReactQuill from 'react-quill'
 
 const ContentEditor = forwardRef((props, ref) => {
-  const [editorState, setEditorState] = useState(() =>
-    EditorState.createEmpty()
-  )
-  const [isTextColorPicker, toggleIsTextColorPicker] = useToggle(false)
-  const [isBgColorPicker, toggleIsBgColorPicker] = useToggle(false)
-  const textColorPickerRef = useRef()
-  const bgColorPickerRef = useRef()
-  const INLINE_TYPES = ['BOLD', 'ITALIC', 'UNDERLINE', 'STRIKETHROUGH']
-  const BLOCK_TYPES = [
-    'unordered-list-item',
-    'ordered-list-item',
-    'blockquote',
-    'code-block'
-  ]
+  const [content, setContent] = useState('')
+
+  const modules = {
+    toolbar: [
+      [
+        { header: [2, 3, 4, 5, 6, false] },
+        { size: ['small', false, 'large', 'huge'] }
+      ],
+
+      [
+        'bold',
+        'italic',
+        'underline',
+        'strike',
+        { color: [] },
+        { background: [] }
+      ],
+
+      ['link', 'image', 'video', 'formula', 'blockquote', 'code-block'],
+
+      [{ align: [] }, { direction: 'rtl' }, { indent: '-1' }, { indent: '+1' }],
+
+      [{ list: 'ordered' }, { list: 'bullet' }, { list: 'check' }],
+
+      [{ script: 'sub' }, { script: 'super' }],
+
+      ['clean']
+    ]
+  }
 
   useImperativeHandle(ref, () => ({
-    getContent: () => {
-      const contentState = editorState.getCurrentContent()
-      return convertToRaw(contentState)
-    },
+    getContent: () => {},
     getEditorState: () => editorState
   }))
 
-  const onToggleInlineStyle = (inlineStyle) => {
-    setEditorState(RichUtils.toggleInlineStyle(editorState, inlineStyle))
+  const handleContentChange = (value) => {
+    setContent(value)
   }
-
-  const onToggleBlockStyle = (blockStyle) => {
-    setEditorState(RichUtils.toggleBlockType(editorState, blockStyle))
-  }
-
-  const handleKeyCommand = (command) => {
-    const newState = RichUtils.handleKeyCommand(editorState, command)
-    if (newState) {
-      setEditorState(newState)
-      return 'handled'
-    }
-    return 'not-handled'
-  }
-
-  const handleFontSizeChange = (event) => {
-    onToggleBlockStyle(event.target.value)
-  }
-
-  const getActiveBlockType = () => {
-    const selection = editorState.getSelection()
-
-    const contentState = editorState.getCurrentContent()
-    const block = contentState.getBlockForKey(selection.getStartKey())
-
-    return block.getType()
-  }
-
-  const isBlockStyleActive = (style) => {
-    const type = getActiveBlockType()
-
-    return type === style
-  }
-
-  const isInlineStyleActive = (style) => {
-    const currentStyle = editorState.getCurrentInlineStyle()
-    return currentStyle.has(style)
-  }
-
-  const addColor = (colorKey) => {
-    onToggleInlineStyle(colorKey)
-
-    if (isTextColorPicker) {
-      toggleIsTextColorPicker()
-    } else if (isBgColorPicker) {
-      toggleIsBgColorPicker()
-    }
-  }
-
-  const toggleColorPicker = (type) => {
-    if (!isTextColorPicker || !isBgColorPicker) {
-      switch (type) {
-        case 'TEXT':
-          toggleIsTextColorPicker()
-
-          if (isBgColorPicker) toggleIsBgColorPicker()
-          break
-        case 'BACKGROUND':
-          toggleIsBgColorPicker()
-
-          if (isTextColorPicker) toggleIsTextColorPicker()
-          break
-      }
-    }
-  }
-
-  const handleWindowClick = (event) => {
-    const target = event.target
-    const isTextColor = textColorPickerRef.current.contains(target)
-    const isBgColor = bgColorPickerRef.current.contains(target)
-
-    if (!isTextColor && isTextColorPicker) {
-      toggleIsTextColorPicker()
-    } else if (!isBgColor && isBgColorPicker) {
-      toggleIsBgColorPicker()
-    }
-  }
-
-  useEffect(() => {
-    window.addEventListener('click', handleWindowClick)
-
-    return () => {
-      window.removeEventListener('click', handleWindowClick)
-    }
-  }, [handleWindowClick])
 
   return (
     <div className="ContentEditor">
-      <div className="toolbar">
-        <div className="btns-wrap">
-          <select value={getActiveBlockType()} onChange={handleFontSizeChange}>
-            <option value="header-one">Header 1</option>
-            <option value="header-two">Header 2</option>
-            <option value="header-three">Header 3</option>
-            <option value="paragraph">Normal</option>
-          </select>
-        </div>
-
-        <div className="btns-wrap">
-          {INLINE_TYPES.map((stlye) => (
-            <EditorBtn
-              key={stlye}
-              className={isInlineStyleActive(stlye) ? 'active' : ''}
-              btnFor={stlye}
-              onMouseDown={() => onToggleInlineStyle(stlye)}
-            />
-          ))}
-
-          <div className="color-picker" ref={textColorPickerRef}>
-            <EditorBtn
-              btnFor="TEXT_COLOR"
-              onMouseDown={() => toggleColorPicker('TEXT')}
-            />
-
-            {isTextColorPicker === true && (
-              <EditorColorPicker type="TEXT" onMouseDown={addColor} />
-            )}
-          </div>
-
-          <div className="color-picker" ref={bgColorPickerRef}>
-            <EditorBtn
-              btnFor="BACKGROUND"
-              onMouseDown={() => toggleColorPicker('BACKGROUND')}
-            />
-
-            {isBgColorPicker === true && (
-              <EditorColorPicker type="BACKGROUND" onMouseDown={addColor} />
-            )}
-          </div>
-        </div>
-
-        <div className="btns-wrap">
-          {BLOCK_TYPES.map((style) => (
-            <EditorBtn
-              key={style}
-              className={isBlockStyleActive(style) ? 'active' : ''}
-              btnFor={style.replaceAll('-', '_')}
-              onMouseDown={() => onToggleBlockStyle(style)}
-            />
-          ))}
-        </div>
-      </div>
-
-      <Editor
-        editorState={editorState}
-        onChange={setEditorState}
-        handleKeyCommand={handleKeyCommand}
-        customStyleMap={editorStyleMap}
+      <ReactQuill
+        theme="snow"
+        value={content}
+        onChange={handleContentChange}
+        modules={modules}
       />
     </div>
   )
