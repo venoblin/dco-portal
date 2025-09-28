2f330c715c6fcbc193713c39c487f2bf77d0c5bb
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2025-09-28 19:39:14 -0400

    modified start script

--- runApp.sh
+++ runApp.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-npm install express sequelize sqlite3 cors dotenv bcrypt
-npm install --save-dev nodemon
-
-cd frontend
-
-npm install react react-dom react-router-dom@6 quill
-npm install --save-dev parcel
-
-cd ../
 
 npm start
---
+++ runInstall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+npm install express sequelize sqlite3 cors dotenv bcrypt
+npm install --save-dev nodemon
+
+cd frontend
+
+npm install react react-dom react-router-dom@6 quill
+npm install --save-dev parcel
