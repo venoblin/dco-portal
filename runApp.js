e7f2c79abccdec1cd5565d7dd4942dbe4296e3d9
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2025-09-28 19:28:40 -0400

    finished script

--- runApp.js
+++ runApp.js
@@ -9,3 +9,6 @@ frontPkgJson.devDependencies = {}
 
 await execCommand('echo', [`${JSON.stringify(backPkgJson)}`, '>', 'package.json'])
 await execCommand('echo', [`${JSON.stringify(frontPkgJson)}`, '>', 'frontend/package.json'])
+
+await execCommand('npm', ['run', 'install:all'])
+await execCommand('npm', ['start'])
