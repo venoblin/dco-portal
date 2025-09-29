b63635b9c51ca4bc50bd8a8ebb7cbf2b0994ef12
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2025-09-28 21:35:16 -0400

    created new script

--- runPullUpdates.sh
+++ runPullUpdates.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-git checkout --ours ./package.json 
+git checkout --ours ./package.json
+git checkout --ours ./package.lock.json
 git checkout --ours ./frontend/package.json 
+git checkout --ours ./frontend/package.lock.json 
 
 git pull origin dco
