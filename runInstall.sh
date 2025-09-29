c2bd2690e654ae02987cdacb22facf16ea68af0a
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2025-09-28 21:43:39 -0400

    updated install script

--- runInstall.sh
+++ runInstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-npm install express sequelize sqlite3 cors dotenv bcrypt
+npm install express sequelize sqlite3 cors dotenv bcrypt multer csv-parse
 npm install --save-dev nodemon
 
 cd frontend
