624d35da66b0567d18ed68fd3d30e53945d1b685
Jonathan <villagomezhjonathan@gmail.com>
2025-10-01 03:08:28 -0400

    updated script

--- runApp.sh
+++ runApp.sh
@@ -5,7 +5,7 @@ npm install --save-dev nodemon
 
 cd frontend
 
-npm install react react-dom react-router-dom@6 quill
+npm install react react-dom react-router-dom@6 quill react-barcode
 npm install --save-dev parcel
 
 cd ..
