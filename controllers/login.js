ae01f7a821f185f844bff643406a5517c7a12b8d
Jonathan <villagomezhjonathan@gmail.com>
2025-10-01 20:10:36 -0400

    created login

--- controllers/login.js
+++ controllers/login.js
@@ -1,17 +1,24 @@
 const service = require('../services/login')
 
-const login = async () => {
+const login = async (req, res) => {
+  const { username, password } = req.body
+
   try {
     if (process.env.AUTH_API) {
-      await service.loginUser()
-    } else if (
-      process.env.USERNAME &&
-      process.env.PASSWORD &&
-      process.env.TOKEN
-    ) {
+      await service.loginUser(username, password)
+    } else {
+      res.status(400).json({
+        error: 'Invalid API url'
+      })
     }
+
+    res.status(400).json({
+      error: 'Password or username incorrect'
+    })
   } catch (error) {
-    console.log(error)
+    res.status(500).json({
+      error: 'Failed to login user'
+    })
   }
 }
 
