bb362f7c5065bb7e24aaead1929684a10ff3f95f
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2025-10-02 17:44:44 -0400

    commit

--- services/login.js
+++ services/login.js
@@ -1,8 +1,9 @@
 const qs = require('querystring')
-const http = require('https')
+import { filterHeaders } from '../utils'
 
 const loginUser = async (req) => {
   const { username, password } = req.body
+  const authApiUrl = `https://${process.env.HOSTANME}${process.env.PATH}`
 
   const excludedHeaders = [
     'content-encoding',
@@ -11,43 +12,45 @@ const loginUser = async (req) => {
     'connection'
   ]
 
-  const options = {
-    method: 'POST',
-    hostname: process.env.HOSTANME,
-    port: null,
-    path: process.env.PATH,
-    headers: {
-      'content-type': 'application/x-www-form-urlencoded'
-    },
-    'cache-control': 'no-cache',
-    'postman-token': process.env.POSTMAN_TOKEN
-  }
-
-  let response
-  const request = http.request(options, function (res) {
-    const chunks = []
-
-    res.on('data', function (chunk) {
-      chunks.push(chunk)
-    })
-    res.on('end', function () {
-      const body = Buffer.concat(chunks)
-      response = body
-    })
+  const bodyString = qs.stringify({
+    grant_type: 'password',
+    client_id: process.env.CLIENT_ID,
+    resource: process.env.RESOURCE,
+    username: `NAEAST\\${username}`,
+    password: password
   })
 
-  request.write(
-    qs.stringify({
-      grant_type: 'password',
-      client_id: process.env.CLIENT_ID,
-      resource: process.env.RESOURCE,
-      username: `NAEAST\\${username}`,
-      password: password
-    })
-  )
-  request.end()
+  const externalResponse = await fetch(authApiUrl, {
+    method: 'POST',
+    body: bodyString,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    redirect: 'manual'
+  })
 
-  return response
+  const externalHeaders = filterHeaders(
+    externalResponse.headers.raw(),
+    excludedHeaders
+  )
+  const externalStatus = externalResponse.status
+
+  if (externalStatus >= 300 && externalStatus < 400) {
+    return {
+      success: false,
+      message: `External service redirect (${externalStatus})`,
+      headers: externalHeaders
+    }
+  }
+
+  const content = await externalResponse.json().catch(() => null)
+
+  return {
+    content: content,
+    statusCode: externalStatus,
+    headers: externalHeaders,
+    success: true
+  }
 }
 
 module.exports = {
