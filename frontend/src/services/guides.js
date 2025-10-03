d5976ac5ef81e2ac9b4509e7461c72bff51b977c
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2025-10-03 13:44:41 -0400

    removed try blocks

--- frontend/src/services/guides.js
+++ frontend/src/services/guides.js
@@ -1,46 +1,30 @@
 import { api } from '../api'
 
 export const postGuide = async (payload) => {
-  try {
-    const res = await api.post('/guides', payload)
+  const res = await api.post('/guides', payload)
 
-    return res
-  } catch (error) {
-    throw new Error(error)
-  }
+  return res
 }
 
 export const getAllGuides = async (query) => {
-  try {
-    let res
-    if (query) {
-      res = await api.get(`/guides?${query}`)
-    } else {
-      res = await api.get('/guides')
-    }
-
-    return res
-  } catch (error) {
-    throw new Error(error)
+  let res
+  if (query) {
+    res = await api.get(`/guides?${query}`)
+  } else {
+    res = await api.get('/guides')
   }
+
+  return res
 }
 
 export const getSingleGuide = async (id) => {
-  try {
-    const res = await api.get(`/guides/${id}`)
+  const res = await api.get(`/guides/${id}`)
 
-    return res
-  } catch (error) {
-    throw new Error(error)
-  }
+  return res
 }
 
 export const getGuidesByTitle = async (title) => {
-  try {
-    const res = await api.get(`/guides/search?title=${title}`)
+  const res = await api.get(`/guides/search?title=${title}`)
 
-    return res
-  } catch (error) {
-    throw new Error(error)
-  }
+  return res
 }
--- frontend/src/services/tools.js
+++ frontend/src/services/tools.js
@@ -1,15 +1,11 @@
 import { api } from '../api'
 
 export const uploadCsv = async (formData, credentials) => {
-  try {
-    const res = await api.upload(
-      '/tools/parse-csv',
-      formData,
-      credentials.accessToken
-    )
+  const res = await api.upload(
+    '/tools/parse-csv',
+    formData,
+    credentials.accessToken
+  )
 
-    return res
-  } catch (error) {
-    throw new Error(error)
-  }
+  return res
 }
