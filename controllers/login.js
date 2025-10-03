9e9589485bda3d9c42839071c13bd865fafa294d
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2025-10-02 21:15:41 -0400

    worked on sending token to backend

--- controllers/login.js
+++ controllers/login.js
@@ -3,6 +3,7 @@ const service = require('../services/login')
 const login = async (req, res) => {
   try {
     const authRes = await service.loginUser(req)
+    console.log(authRes)
 
     res.status(200).json({
       data: {
