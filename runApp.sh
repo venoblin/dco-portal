75ba5e1143b7fd7e00653f11f7cb92b051a54431
Jonathan <villagomezhjonathan@gmail.com>
2025-10-04 15:51:08 -0400

    worked on spreadsheet

--- runApp.sh
+++ runApp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-npm install express sequelize sqlite3 cors dotenv bcrypt multer csv-parse
+npm install express sequelize sqlite3 cors dotenv bcrypt multer csv-parse @tanstack/react-table
 npm install --save-dev nodemon
 
 cd frontend
