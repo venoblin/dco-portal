87ca1ecce3d4bd4e6d8427ff2f23b6c8248f8a3c
Jonathan <villagomezhjonathan@gmail.com>
2025-10-05 20:38:57 -0400

    removed stacktable

--- runApp.sh
+++ runApp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-npm install express sequelize sqlite3 cors dotenv bcrypt multer csv-parse @tanstack/react-table
+npm install express sequelize sqlite3 cors dotenv bcrypt multer csv-parse
 npm install --save-dev nodemon
 
 cd frontend
