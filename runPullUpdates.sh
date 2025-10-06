cc15f388af47719b0502a6daaa30c86d8bc00cee
Jonathan <villagomezhjonathan@gmail.com>
2025-10-06 14:50:11 -0400

    modified script

--- runPullUpdates.sh
+++ runPullUpdates.sh
@@ -5,6 +5,12 @@ git checkout --ours ./package-lock.json
 git checkout --ours ./frontend/package.json 
 git checkout --ours ./frontend/package-lock.json 
 
-current_branch=$(git symbolic-ref --short HEAD)
 
-git pull origin $current_branch
+if [[ -n $1 ]]; then
+  git pull origin $1
+
+  else
+  current_branch=$(git symbolic-ref --short HEAD)
+  git pull origin $current_branch
+  fi
+fi
