12e685158e73fb6849c54fb0c0091730fdd28f86
Jonathan <villagomezhjonathan@gmail.com>
2025-10-05 20:38:21 -0400

    removed stacktable

--- frontend/src/components/Spreadsheet.jsx
+++ frontend/src/components/Spreadsheet.jsx
@@ -1,22 +1,9 @@
 import './Spreadsheet.css'
-import {
-  useReactTable,
-  flexRender,
-  getCoreRowModel,
-  getFilteredRowModel
-} from '@tanstack/react-table'
 
 const Spreadsheet = (props) => {
   if (!props.rowData) {
     return null
   }
-
-  const table = useReactTable({
-    data: props.rowData,
-    columns: props.columns,
-    getCoreRowModel: getCoreRowModel(),
-    getFilteredRowModel: getFilteredRowModel()
-  })
 
   return (
     <div className={`Spreadsheet${props.isCopyClick ? ' copied' : ''}`}>
@@ -26,32 +13,15 @@ const Spreadsheet = (props) => {
         )}
 
         <thead>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <th key={header.id} colSpan={header.colSpan}>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
-                      )}
-                </th>
-              ))}
-            </tr>
-          ))}
+          <tr>
+            <th></th>
+          </tr>
         </thead>
 
         <tbody>
-          {table.getRowModel().rows.map((row) => (
-            <tr key={row.id}>
-              {row.getVisibleCells().map((cell) => (
-                <td key={cell.id}>
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
-              ))}
-            </tr>
-          ))}
+          <tr>
+            <td></td>
+          </tr>
         </tbody>
       </table>
 
