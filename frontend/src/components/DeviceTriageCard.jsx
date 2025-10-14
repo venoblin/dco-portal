c86025caab457b0bf18dea59be19e3524ecc54ae
Jonathan <villagomezhjonathan@gmail.com>
2025-10-14 02:19:48 -0400

    created add path form

--- frontend/src/components/DeviceTriageCard.jsx
+++ frontend/src/components/DeviceTriageCard.jsx
@@ -39,7 +39,7 @@ const DeviceTriageCard = (props) => {
               required
             />
 
-            <div>
+            <div className="checkbox-wrap">
               <label
                 className="show"
                 htmlFor={`isPortActice_${props.device.id}`}
@@ -55,6 +55,21 @@ const DeviceTriageCard = (props) => {
               />
             </div>
           </div>
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={30}
+            height={30}
+            viewBox="0 0 24 24"
+          >
+            <g fill="none">
+              <path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.019-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"></path>
+              <path
+                fill="#989898"
+                d="m15.06 5.283l5.657 5.657a1.5 1.5 0 0 1 0 2.12l-5.656 5.658a1.5 1.5 0 0 1-2.122-2.122l3.096-3.096H4.5a1.5 1.5 0 0 1 0-3h11.535L12.94 7.404a1.5 1.5 0 0 1 2.122-2.121Z"
+              ></path>
+            </g>
+          </svg>
 
           <div className="form-section">
             <label htmlFor={`destHostname_${props.device.id}`}>
@@ -79,7 +94,7 @@ const DeviceTriageCard = (props) => {
               onChange={onDestPortChange}
             />
 
-            <div>
+            <div className="checkbox-wrap">
               <label
                 className="show"
                 htmlFor={`destIsPortActive_${props.device.id}`}
