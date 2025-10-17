dd54932f141ac4dea7b377c364cf2d30ac15384f
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2025-10-16 21:23:27 -0400

    worked on hop ui

--- frontend/src/components/HopTriageCard.jsx
+++ frontend/src/components/HopTriageCard.jsx
@@ -3,6 +3,10 @@ import './HopTriageCard.css'
 const HopTriageCard = (props) => {
   return (
     <div className="HopTriageCard">
+      <div className="inputs">
+        <button>Edit Hop</button>
+        <button className="danger-bg">Delete Hop</button>
+      </div>
       <p>{props.hop.hop}</p>
     </div>
   )
