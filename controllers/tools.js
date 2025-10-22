1558b71884366b827785b894ddd695c373ff77a9
Jonathan <villagomezhjonathan@gmail.com>
2025-10-22 00:35:45 -0400

    fixed assetname

--- controllers/tools.js
+++ controllers/tools.js
@@ -99,7 +99,7 @@ const findAllDevices = async (req, res) => {
         newDevice.info = device
       } else {
         newDevice.info = {
-          assetName: 'Not Found',
+          assetName: query,
           assetTag: 'Not Found',
           invNo: 'Not Found',
           deployment: {
