3cbdcf60601b96d18823ec004bf2cfcba8527a3c
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2025-10-26 19:30:04 -0400

    added react spreadsheet

--- frontend/src/components/Spreadsheet.jsx
+++ frontend/src/components/Spreadsheet.jsx
@@ -1,64 +1,21 @@
-import './Spreadsheet.css'
-import {
-  useReactTable,
-  flexRender,
-  getCoreRowModel,
-  getFilteredRowModel
-} from '@tanstack/react-table'
+import { Spreadsheet as ReactSpreadsheet } from 'react-spreadsheet'
+import { useState } from 'react'
 
 const Spreadsheet = (props) => {
-  if (!props.rowData) {
-    return null
-  }
+  const [data, setData] = useState([
+    [{ value: 'A1' }, { value: 'B1' }],
+    [{ value: 'A2' }, { value: 'B2' }]
+  ])
+
   const classes = `Spreadsheet to-print light${
     props.isCopyClick ? ' copied' : ''
   } ${props.className ? props.className : ''}`
 
-  const table = useReactTable({
-    data: props.rowData,
-    columns: props.columns,
-    getCoreRowModel: getCoreRowModel(),
-    getFilteredRowModel: getFilteredRowModel()
-  })
+  const [selected, setSelected] = useState([])
 
   return (
     <div className={classes}>
-      {props.isCopyClick === true && (
-        <div className="copy-msg">Successfully copied!</div>
-      )}
-
-      <div className="table-wrap light">
-        <table ref={props.tableRef}>
-          <thead>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <tr key={headerGroup.id}>
-                {headerGroup.headers.map((header) => (
-                  <th key={header.id} colSpan={header.colSpan}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </thead>
-
-          <tbody>
-            {table.getRowModel().rows.map((row) => (
-              <tr key={row.id}>
-                {row.getVisibleCells().map((cell) => (
-                  <td key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      <ReactSpreadsheet data={data} onChange={setData} onSelect={setSelected} />
     </div>
   )
 }
