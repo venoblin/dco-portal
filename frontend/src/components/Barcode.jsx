e0aa399eec0bf269ce36a90d990cc9df34a5298b
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2025-11-09 13:07:28 -0500

    worked on barcodes

--- frontend/src/components/Barcode.jsx
+++ frontend/src/components/Barcode.jsx
@@ -6,13 +6,17 @@ function Barcode(props) {
   const barcodeRef = useRef(null)
 
   useEffect(() => {
-    if (barcodeRef.current) {
+    const height = props.height ? props.height : 14
+    const width = props.width ? props.width : 0.8
+    const fontSize = props.fontSize ? props.fontSize : 14
+
+    if (barcodeRef.current && props.value !== '') {
       JsBarcode(barcodeRef.current, props.value, {
         displayValue: true,
         text: props.value,
-        height: 14,
-        width: 0.8,
-        fontSize: 14,
+        height: height,
+        width: width,
+        fontSize: fontSize,
         lineColor: 'black',
         background: 'transparent'
       })
