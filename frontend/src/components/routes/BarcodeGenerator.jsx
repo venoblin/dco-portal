9a47cb096dd4156704d53f98ac27c6279a59e31d
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2025-11-08 22:21:59 -0500

    created new route

---
+++ frontend/src/components/routes/BarcodeGenerator.jsx
@@ -0,0 +1,11 @@
+const BarcodeGenerator = () => {
+  return (
+    <div className="BarcodeGenerator">
+      <header>
+        <h1>Barcode Generator</h1>
+      </header>
+    </div>
+  )
+}
+
+export default BarcodeGenerator
